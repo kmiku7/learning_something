eb619be2b652bd222dc652f75e61bdef6a9ee443
kmiku7 <kakoimiku@gmail.com>
2022-01-09 15:57:56 +0800

    add dagster demo jobs

---
+++ dagster/complex_job/README.md
@@ -0,0 +1,14 @@
+
+# Execute it by ...
+## dagit
+In LaunchPad, input:
+```
+ops:
+  download_csv:
+    config:
+      url: "https://docs.dagster.io/assets/cereal.csv"
+```
+
+## JobDefinition.execute_in_process
+
+## dagster job execute
---
+++ dagster/complex_job/hello_cereal_diamond.py
@@ -0,0 +1,61 @@
+import csv
+
+import requests
+from dagster import get_dagster_logger, job, op
+
+
+@op(config_schema={'url': str})
+def download_csv(context):
+    response = requests.get(context.op_config['url'])
+    lines = response.text.split("\n")
+    return [row for row in csv.DictReader(lines)]
+
+
+@op
+def download_cereals():
+    response = requests.get("https://docs.dagster.io/assets/cereal.csv")
+    lines = response.text.split("\n")
+    return [row for row in csv.DictReader(lines)]
+
+
+@op
+def find_highest_calorie_cereal(cereals):
+    sorted_cereals = list(
+        sorted(cereals, key=lambda cereal: cereal["calories"]))
+    return sorted_cereals[-1]["name"]
+
+
+@op
+def find_highest_protein_cereal(cereals):
+    sorted_cereals = list(sorted(cereals,
+                                 key=lambda cereal: cereal["protein"]))
+    return sorted_cereals[-1]["name"]
+
+
+@op
+def display_results(most_calories, most_protein):
+    logger = get_dagster_logger()
+    logger.info(f"Most caloric cereal: {most_calories}")
+    logger.info(f"Most protein-rich cereal: {most_protein}")
+
+
+@job
+def diamond():
+    cereals = download_csv()
+    display_results(
+        most_calories=find_highest_calorie_cereal(cereals),
+        most_protein=find_highest_protein_cereal(cereals),
+    )
+
+
+if __name__ == '__main__':
+    result = diamond.execute_in_process(
+        run_config={
+            "ops": {
+                "download_csv": {
+                    "config": {
+                        "url": "https://docs.dagster.io/assets/cereal.csv"
+                    }
+                }
+            }
+        })
